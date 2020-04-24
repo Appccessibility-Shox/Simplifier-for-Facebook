c3adb3cea0828be49439d54a620ac0824ddb5e59
Reiny Botros <reinybo@gmail.com>
2020-04-23 20:40:31 -0400

    Added an extension description and an app category to the info.plist.

--- Simplifier for Facebook/AppDelegate.swift
+++ Simplifier for Facebook/AppDelegate.swift
@@ -11,12 +11,9 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
 
 }
 
